bd1288df94eb4174406378d9d914c5dc823efb92
haritha14hari <harithapamula@gmail.com>
2023-09-20 12:47:47 +0530

    space added by haritha

--- src/test/java/haripackage/swapnumbers.java
+++ src/test/java/haripackage/swapnumbers.java
@@ -2,7 +2,7 @@ package haripackage;
 
 public class swapnumbers {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
 
 	}
